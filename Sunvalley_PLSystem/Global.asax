9379787b30db48632760cea326c97f9c84f09f7d
Gibran Amparan <GibranAmparanD@hotmail.com>
2016-03-05 13:09:30 -0700

    First commit

---
+++ Sunvalley_PLSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sunvalley_PLSystem.MvcApplication" Language="C#" %>
---
+++ Sunvalley_PLSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sunvalley_PLSystem.Startup))]
+namespace Sunvalley_PLSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
