e23ae6c78d0337ee65e02d8e097ae69627602984
Rangel <erick_1095@hotmail.com>
2016-03-08 22:02:54 -0700

    entidades completas

--- Sunvalley_PLSystem/Models/House.cs
+++ Sunvalley_PLSystem/Models/House.cs
@@ -10,9 +10,26 @@ namespace Sunvalley_PLSystem.Models
     {
         [Key]
         public int houseID { get; set; }
+        [Display(Name ="Home Name")]
+        public string name { get; set; }
+        [Display(Name = "Status")]
+        public Boolean status { get; set; }
+        [Display(Name = "Created")]
+        public DateTime created { get; set; }
+        [Display(Name = "Area")]
+        public string area { get; set; }
+        [Display(Name = "Adress")]
+        public string adress { get; set; }
+        [Display(Name = "City/Area")]
+        public string cityArea { get; set; }
+        [Display(Name = "Country")]
+        public string country { get; set; }
+        [Display(Name = "State/Province")]
+        public string stateProvince { get; set; }
+        [Display(Name = "Postal Code")]
+        public int postalCode { get; set; }
 
-
-        public virtual RegisterViewModel owner { get; set; }
+        public virtual RegisterViewModel primaryOwner { get; set; }
         public virtual ICollection<Movement> movimientos { get; set; }
     }
 }
