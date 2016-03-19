c543c1619a64e079fb470d768ffeb7b2546bdb8e
juansaul <juan.saul2@live.com.mx>
2016-03-19 14:16:43 -0600

    Contraseña modificada

--- Sunvalley_PLSystem/Models/AccountViewModels.cs
+++ Sunvalley_PLSystem/Models/AccountViewModels.cs
@@ -128,7 +128,7 @@ namespace Sunvalley_PLSystem.Models
         public string Email2 { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        //[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
