dc461bae0c5f833fe459cda59771f84017d34aee
juansaul <juan.saul2@live.com.mx>
2016-03-22 16:28:50 -0600

    f

--- Sunvalley_PLSystem/Models/AccountViewModels.cs
+++ Sunvalley_PLSystem/Models/AccountViewModels.cs
@@ -48,13 +48,18 @@ namespace Sunvalley_PLSystem.Models
     }
 
     public class LoginViewModel
-    {
+
+
+
+{
+       
+
         [Required]
         [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
 
-        [Required]
+      
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
