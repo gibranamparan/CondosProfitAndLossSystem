f5d212434358bd0ef9d6a3ae7c9ca7c7dc56d694
juansaul <juan.saul2@live.com.mx>
2016-04-02 12:43:01 -0600

    platilla agregada y limpiada

--- Sunvalley_PLSystem/App_Start/BundleConfig.cs
+++ Sunvalley_PLSystem/App_Start/BundleConfig.cs
@@ -31,17 +31,30 @@ namespace Sunvalley_PLSystem
             /*Plantilla*/
                 /*Estilos*/
             bundles.Add(new StyleBundle("~/app").Include(
-                      "~/app/css/bootstrap.css"));
+                      "~/app/css/bootstrap.css",
+                      "~/app/css/app.css"));
 
 
             bundles.Add(new StyleBundle("~/vendor").Include(
-                      ""));
+                      "~/vendor/fontawesome/css/font-awesome.min.css",
+                      "~/vendor/animo/animate_animo.css",
+                      "~/vendor/csspinner/csspinner.min.css"));
 
             /*Scripts*/
-            bundles.Add(new ScriptBundle("~/app").Include(
-                      ""));
-            bundles.Add(new ScriptBundle("~/vendor").Include(
-                      ""));
+            bundles.Add(new ScriptBundle("~/appS").Include(
+                      "~/app/js/app.js"));
+
+            bundles.Add(new ScriptBundle("~/vendorS").Include(
+                        "~/vendor/modernizr/modernizr.js",
+                      "~/vendor/fastclick/fastclick.js",
+                      "~/vendor/jquery/jquery.min.js",
+                      "~/vendor/bootstrap/js/bootstrap.min.js",
+                      "~/vendor/chosen/chosen.jquery.min.js",
+                      "~/vendor/slider/js/bootstrap-slider.js",
+                      "~/vendor/filestyle/bootstrap-filestyle.min.js",
+                      "~/vendor/animo/animo.min.js",
+                      "~/vendor/sparklines/jquery.sparkline.min.js",
+                      "~/vendor/slimscroll/jquery.slimscroll.min.js"));
 
         }
     }
